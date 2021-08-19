7a6f42e3f21c5eac309fe8eca8557c34936aae7d
Julia Putko <jputko@aeolus.seos.uvic.ca>
2021-08-18 21:38:39 -0700

    synch

--- src/models/uvic/npzd_resolute.F90
+++ src/models/uvic/npzd_resolute.F90
@@ -189,10 +189,12 @@
    call self%get_parameter(self%zo2_0, 'zo2_0','umol/L', 'zo2 initial value', default=0.1_rk)
    call self%get_parameter(self%nh4_0, 'nh4_0','umol/L', 'nh4 initial value', default=10.0_rk)
    call self%get_parameter(self%no3_0, 'no3_0','umol/L', 'no3 initial value', default=10.0_rk)
-   call self%get_parameter(self%de1_0, 'de1_0','umol/L','de1 initial value ', default=1.0_rk)
-   call self%get_parameter(self%de2_0, 'de2_0','umol/L', 'de2 initial value', default=1.0_rk)
-   call self%get_parameter(self%bsi_0, 'bsi_0','umol/L', 'bsi initial value', default=1.0_rk)
-   call self%get_parameter(self%sil_0, 'sil_0 ','umol/L', 'sil initial value', default=5.0_rk)
+   !call self%get_parameter(self%de1_0, 'de1_0','umol/L','de1 initial value ', default=1.0_rk)
+  ! call self%get_parameter(self%de2_0, 'de2_0','umol/L', 'de2 initial value', default=1.0_rk)
+   !call self%get_parameter(self%bsi_0, 'bsi_0','umol/L', 'bsi initial value', default=1.0_rk)
+   call self%get_parameter(self%sil_0, 'det','', 'det', default=0.0_rk)
+   call self%get_parameter(self%sil_0, 'sil_0','', 'sil initial value', default=5.0_rk)
+   call self%get_parameter(self%sil_0, 'psi_0','', '', default=0.0_rk)
 
    if (self%use_icealgae) then !read in icealgae model vars 
    !call self%get_parameter(self%r_pond, 'r_pond','', 'melt pond drainage rate', default=0.0175_rk)
